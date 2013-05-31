755951d623874020e505649db4296c6daf9c18c2
Daniel McQuillan <dmcquillan@ahold.com>
2013-05-30 23:24:41 -0500

    Update of HQL queries

--- src/main/java/com/youthministry/domain/Group.java
+++ src/main/java/com/youthministry/domain/Group.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
+@Entity(name="GROUP_DETAIL")
 @Table(name="GROUP_DETAIL")
 public class Group {
 	@Id
